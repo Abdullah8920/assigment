e5c142f2df4a4a536b422a59a0d9a4d1b1b55f3c
Abdullah Mushtaq <abdullahansari7463@gmail.com>
2025-11-04 20:56:15 -0800

    first commit

---
+++ inedx.js
@@ -0,0 +1,120 @@
+//alert
+
+//1
+// alert("Hello Abdullah")
+
+//2
+// alert("Invalid Password")
+
+//3
+// let name = "Abdullah";
+// let favoriteColor = "Blue";
+
+// alert("My name is " + name + " and my favorite color is " + favoriteColor + "!");
+
+//4
+// var username ="Abdullah"
+// alert(username)
+
+//5
+// let message = "Hello World";
+// console.log(message);
+
+//6
+// var stuname="Umer"
+// let age = "12"
+// let grade ="9"
+// alert("Student name" + stuname + "Age" + age + "Grade" + grade)
+
+//7
+// let myAge = 20;
+// alert(myAge);
+
+//8
+// let english = 85;
+// let math = 90;
+// let science = 80;
+// let computer = 95;
+// let urdu = 88;
+
+// let total = english + math + science + computer + urdu;
+
+// alert("Total Marks: " + total);
+
+//9
+// let temperature = 10;
+// alert("The temperature is " + temperature + "°C");
+
+//10
+// legeal
+// var name="Abdullah"
+// var city="Karachi"
+// var place="K-2"
+// var age="10"
+// var object="spoon"
+
+//11
+// special character can't be used
+// reservedword (if,else,var)can't be used as variable
+// Special characters not allowed
+
+//12
+var myName = "Abdullah"
+console.log(myName)
+
+//13
+var $age = "19"
+console.log($age)
+
+// 14
+var _object = "spoon"
+console.log(_object)
+
+// 15
+// var num1= 80;
+// var num2= 20;
+
+// var sum =num1 + num2
+
+// alert("The Sum is:" + sum)
+
+// 16
+// var sub1 = 10;
+// var sub2 = 5;
+
+// var subtract = sub1-sub2
+// alert(subtract)
+
+// 17
+// var multi1 = 10;
+// var multi2 = 5;
+
+// var Multiply = multi1*multi2
+// alert(Multiply)
+
+// 18
+// var divi1 = 10;
+// var divi2 = 5;
+
+// var divide = divi1/divi2
+// alert(divide)
+
+// 19s
+var modulus1 = 10;
+var modulus2 = 5;
+
+var Modulus = modulus1%modulus2
+alert(Modulus)
+
+// 20
+   var a = 5
+var b = a++
+console.log(a)
+
+var a = 7
+console.log(++a)
+
+// 21
+var c = 10
+var b = c--
+console.log(c)
