00302e94b9649fe00798a5e7c0e598ec74dff060
Abdullah Mushtaq <abdullahansari7463@gmail.com>
2025-11-16 02:30:16 -0800

    first commit

--- inedx.js
+++ inedx.js
@@ -100,21 +100,189 @@ console.log(_object)
 // alert(divide)
 
 // 19s
-var modulus1 = 10;
-var modulus2 = 5;
+// var modulus1 = 10;
+// var modulus2 = 5;
 
-var Modulus = modulus1%modulus2
-alert(Modulus)
+// var Modulus = modulus1%modulus2
+// alert(Modulus)
 
-// 20
-   var a = 5
-var b = a++
-console.log(a)
+// // 20
+//    var a = 5
+// var b = a++
+// console.log(a)
 
-var a = 7
-console.log(++a)
+// var a = 7
+// console.log(++a)
 
-// 21
-var c = 10
-var b = c--
-console.log(c)
+// // 21
+// var c = 10
+// var b = c--
+// console.log(c)
+
+// var greet = "Hello Students"
+// alert(greet)
+
+var name = "Abdullah"
+console.log(name)
+
+// var age = 19
+// alert("My Age Is" + $age)
+
+// var myName = "Abdullah"
+// var lastnamw = "Mushtaq"
+// console.log(myName + lastnamw)
+
+// let city = "Karachi";
+// alert("I live in " + city);
+
+// var myName = "Abdullah"
+// var lastnamw = "Mushtaq"
+// console.log(myName + lastnamw)
+
+// let greeting = "Hello";
+// let friend = "Ali";
+// alert(greeting + " " + friend);
+
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+
+
+// let price = 50;
+// alert(price * 10);
+
+
+// let marks = 17;
+// console.log(marks % 3);
+
+
+let a = 15;
+let b = 7;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+
+var x = 10
+x = x + 5
+x = x - 3
+console.log(x)
+
+
+var num = 5
+console.log(num++)
+console.log(num)
+console.log(++num);
+
+
+let z = 10;
+console.log(z ** 2);
+
+
+let n = 7;
+console.log(n % 4);
+
+console.log(10 + 20 + "30");
+console.log("10" + 20 + 30);
+
+let str = "5";
+let numS = 3;
+
+console.log(str + numS);
+console.log(str * numS);
+
+
+console.log(5 + "2");
+
+
+let a1 = "I";
+let b2 = "love";
+let c = "JavaScript";
+
+console.log(a1 + " " + b2 + " " + c);
+
+
+let fruit = "apple";
+let quantity = 10;
+
+// alert("I have " + quantity + " apples");
+
+// let day = "Friday";
+// let activity = "party";
+
+// alert("On " + day + " I will go to " + activity);
+
+
+// let names = prompt("Enter your name:");
+// alert("Hello " + names + "!");
+
+// let numA = prompt("Enter first number:");
+// let numB = prompt("Enter second number:");
+
+// alert(Number(numA) + Number(numB));
+
+
+var number = 10
+if (number == 10) {
+   console.log("Number is 10")
+}
+
+// var markss = Number(prompt(85))
+// if (markss > 80) {
+//    alert("A++ Grade")
+// }
+
+
+// let marksss = 85;
+
+// if (marksss > 80) {
+//    alert("A grade");
+// }
+
+
+// var adult = Number(prompt(18))
+// if (adult >= 18) {
+//    alert("You are Adult ")
+// } else {
+//    alert("You are  Minor")
+// }
+
+
+let a3 = 5;
+let b3 = 10;
+
+console.log("a > b:", a3 > b3);
+console.log("a < b:", a3 < b3);
+console.log("a >= b:", a3 >= b3);
+console.log("a <= b:", a3 <= b3);
+console.log("a == b:", a3 == b3);
+console.log("a === b:", a3 === b3);
+
+
+// let x = 10;
+// let y = "10";
+
+// console.log("x == y:", x == y);
+// console.log("x === y:", x === y);
+
+
+let marks = prompt("Enter your marks:");
+marks = Number(marks);
+
+
+if (marks >= 90) {
+   alert("A+");
+} else if (marks >= 80 && marks <= 89) {
+   alert("A");
+} else if (marks >= 70 && marks <= 79) {
+   alert("B");
+} else if (marks >= 60 && marks <= 69) {
+   alert("C");
+} else if (marks >= 50 && marks <= 59) {
+   alert("D");
+} else {
+   alert("Fail");
+}
